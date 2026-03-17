2fed362c38405f502dae83bfb2af25a7b755b39e
dan <dv.kochetov@yandex.ru>
2026-03-17 14:54:20 +0300

    added routes

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from app.api.routes import router
+
 app = FastAPI(title="Career tracker")
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
+app.include_router(router)
 
 
 @app.get("/")
-def read_root(request: Request):
+async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
