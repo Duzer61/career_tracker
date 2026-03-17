53ab5d29e7eb0cf5f3e5408a755e3e38727e871c
dan <dv.kochetov@yandex.ru>
2026-03-17 14:50:20 +0300

    added static

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import os
 
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="Career tracker")
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 
