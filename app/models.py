60ef6d0bb0f0a985b626f0ac9fa9b307422417b6
dan <dv.kochetov@yandex.ru>
2026-03-17 17:02:44 +0300

    added utils.py

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
-from datetime import datetime, timezone
+from datetime import datetime
 
 from sqlalchemy import DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.types import String
+
+from app.utils import utc_now
 
 Base = declarative_base()
 
@@ -13,7 +15,7 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     login: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    created: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=utc_now)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
 
 
@@ -22,4 +24,5 @@ class Card(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     company_name: Mapped[str] = mapped_column(String(255), nullable=False)
-    created: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=utc_now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=utc_now, onupdate=utc_now)
---
+++ app/utils.py
@@ -0,0 +1,6 @@
+from datetime import datetime, timezone
+
+
+def utc_now() -> datetime:
+    """Возвращает дату и время по UTC"""
+    return datetime.now(timezone.utc)
