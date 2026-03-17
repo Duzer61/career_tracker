9806ce76c1f571c38a153ba2b3270dbf6ff2680f
dan <dv.kochetov@yandex.ru>
2026-03-17 16:50:24 +0300

    modified models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.types import String
@@ -10,4 +13,13 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     login: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    created: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+
+
+class Card(Base):
+    __tablename__ = "cards"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    company_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    created: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
