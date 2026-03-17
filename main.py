0e054d3bc166844e1de19ba8888b36eb59410f27
dan <dv.kochetov@yandex.ru>
2026-03-17 12:38:49 +0300

    modified main.py added fastapi

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
+
+import uvicorn
+
+
 def main():
     print("Hello from career-tracker!")
 
 
 if __name__ == "__main__":
-    main()
+    self_filename = os.path.splitext(os.path.basename(__file__))[0]
+    uvicorn.run(f"{self_filename}:app", host="127.0.0.1", port=8000, reload=True)
