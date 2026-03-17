84e0dec25814ba5ea7b27a2f16e29e6f23377658
dan <dv.kochetov@yandex.ru>
2026-03-17 14:46:57 +0300

    added templates

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="Career tracker")
+templates = Jinja2Templates(directory="app/templates")
 
 
 @app.get("/")
-def read_root():
-    return {"message": "App started"}
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 if __name__ == "__main__":
