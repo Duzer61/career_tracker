01b59fe474439ac8a132e4b3cc6b86c63883103c
dan <dv.kochetov@yandex.ru>
2026-03-18 12:18:25 +0300

    added database.py

---
+++ app/db/database.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.config import config as cf
+
+DB_URL = cf.db.db_url
+
+engine = create_async_engine(DB_URL)
+AcyncLocalSession = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_session():
+    async with AcyncLocalSession() as session:
+        yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
