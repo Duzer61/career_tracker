e52ae831e0d1392b7dda2ae74a5b2886a2e343de
dan <dv.kochetov@yandex.ru>
2026-03-18 12:01:37 +0300

    added config

---
+++ app/config.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class DatabaseConfig:
+    db_url: str
+
+
+@dataclass
+class Config:
+    db: DatabaseConfig
+
+
+def load_config() -> Config:
+    env = Env()
+    env.read_env()
+
+    return Config(
+        db=DatabaseConfig(
+            db_url=f"postgresql+asyncpg://{env("POSTGRES_USER")}:{env("POSTGRES_PASSWORD")}"
+            f"@localhost/{env("POSTGRES_DB")}"
+        )
+    )
+
+
+config = load_config()
