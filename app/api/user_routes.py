3fddc52be8bd50cdc985ae4d9b5942cbf0c0bd06
dan <dv.kochetov@yandex.ru>
2026-03-31 14:41:11 +0300

    added delete user

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request, Response
 from sqlalchemy import select
 
+from app.auth import delete_all_user_sessions, get_current_user
+from app.crud import delete_user
 from app.db.database import SessionDep
 from app.db.models import User
 from app.schemas import UserResponse
@@ -21,3 +23,31 @@ async def get_users(session: SessionDep):
     result = await session.execute(select(User))
     users = result.scalars().all()
     return users
+
+
+@router.get("/me", response_model=UserResponse)
+async def get_current_user_info(current_user: User = Depends(get_current_user)):
+    """
+    Get current user info.
+    """
+    return current_user
+
+
+@router.delete("/me", status_code=204)
+async def delete_current_user(
+    request: Request,
+    response: Response,
+    session: SessionDep,
+    current_user: User = Depends(get_current_user),
+):
+    """
+    Delete current user.
+    """
+    # Delete user from database
+    await delete_user(session, current_user)
+    # Delete all user sessions
+    await delete_all_user_sessions(request)
+    # Delete cookies
+    response.delete_cookie("access_token")
+    response.delete_cookie("refresh_token")
+    return None
--- app/crud.py
+++ app/crud.py
@@ -1,4 +1,4 @@
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.auth import get_password_hash
@@ -12,13 +12,24 @@ async def create_user(session: AsyncSession, user_data: UserCreate) -> User:
     """
     Create a new user.
     """
+    hashed_password = get_password_hash(user_data.password)
+    user = User(login=user_data.login, hashed_password=hashed_password)
+    session.add(user)
     try:
-        hashed_password = get_password_hash(user_data.password)
-        user = User(login=user_data.login, hashed_password=hashed_password)
-        session.add(user)
         await session.commit()
         await session.refresh(user)
         return user
     except IntegrityError:
-        await session.rollback()
         raise ValueError(f"User with login '{user_data.login}' already exists")
+
+
+async def delete_user(session: AsyncSession, current_user: User) -> None:  # TODO: Дописать
+    """
+    Delete a user.
+    """
+    try:
+        await session.delete(current_user)
+        await session.commit()
+    except SQLAlchemyError as e:
+        await session.rollback()
+        raise ValueError(f"Error deleting user: {e}")
